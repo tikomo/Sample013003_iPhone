285c887113a72ccbcf83b290431827e7703d7dd8
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-01-30 23:57:55 +0900

    Initial Commit

---
+++ Sample013003Tests/Sample013003Tests.m
@@ -0,0 +1,34 @@
+//
+//  Sample013003Tests.m
+//  Sample013003Tests
+//
+//  Created by tikomo on 2014/01/30.
+//  Copyright (c) 2014年 tikomo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sample013003Tests : XCTestCase
+
+@end
+
+@implementation Sample013003Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
